be91b33175b3db0a7c71bdc6e94c29fcb721997d
habibor-rahaman1010 <habibor144369@gmail.com>
2023-01-21 22:09:53 +0600

    ok done man

--- All_Class/StudentClass.cs
+++ All_Class/StudentClass.cs
@@ -4,26 +4,31 @@ class Student<Type>
     public Type name;
     public Type email;
     public Type phone;
-    public Type depertment;
+    public Type department;
     public Type[] subjects;
 
-    public Student(Type id, Type name, Type email, Type phone, Type depertment, Type[] subjects)
+    public Student(Type id, Type name, Type email, Type phone, Type department, Type[] subjects)
     {
         this.id = id;
         this.name = name;
         this.email = email;
         this.phone = phone;
-        this.depertment = depertment;
+        this.department = department;
         this.subjects = subjects;
     }
 
     public void DisplayInfo()
     {
-        Console.WriteLine(this.id);
-        Console.WriteLine(this.name);
-        Console.WriteLine(this.email);
-        Console.WriteLine(this.phone);
-        Console.WriteLine(this.depertment);
-        Console.WriteLine(this.subjects);
+        Console.WriteLine("ID: ", this.id);
+        Console.WriteLine("Name: ", this.name);
+        Console.WriteLine("Email ", this.email);
+        Console.WriteLine("Phone", this.phone);
+        Console.WriteLine("Department: ", this.department);
+
+        Console.WriteLine("Subjects: ");
+        foreach (var item in this.subjects)
+        {
+            Console.WriteLine("\tb", item);
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -5,7 +5,14 @@
     {
         static void Main(string[] args)
         {
-            Student<object> student1 = new Student<object>(144369, "Habibor Rahaman", "habibor.rahaman1010@gmail.com", "01768280237", "Computer science and engineering", new string[] { "hello", "mello" });
+            Student<object> student1 = new Student<object>(144369, "Habibor Rahaman", "habibor.rahaman1010@gmail.com", "01768280237", "Computer science and engineering",
+            new string[] {
+                "Data Strucutre and Algorithm",
+                "Database Design",
+                "Oeratinf System Applicatin",
+                "Network and security system",
+                "OOP with c#"
+            });
 
             student1.DisplayInfo();
         }
